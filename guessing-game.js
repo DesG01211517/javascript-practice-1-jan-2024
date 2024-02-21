c12185e8c63965651b5b3be35b132508091ded19
DesG01211517 <desmongarrison2@gmail.com>
2024-02-20 22:17:59 -0500

    started

--- guessing-game.js
+++ guessing-game.js
@@ -1,2 +1,20 @@
 console.log("Guessing Game!");
 console.log("it works!");
+
+startButton.onclick = function () {
+    alert('Ready, player 1?');
+
+    confirm("Let's play a guessing game.");
+
+    let g = prompt("Guess a number...");
+
+    let guess = parseInt(g);
+
+    if(guess === 7) {
+        alert("You got it!");
+    }  else  {
+        alert("No!");
+    }
+
+    alert("Thanks for playing!");
+}
